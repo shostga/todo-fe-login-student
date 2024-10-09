87f498cf733705b084a9d47d6961198b58b791d6
John Seo <shostga@naver.com>
2024-10-09 16:48:32 +0900

    revise2 RegisterPage.js

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
 const RegisterPage = () => {
   const [name, setName] = useState("");
