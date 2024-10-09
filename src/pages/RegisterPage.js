803e3c21882ef799d254a0b991c4efa446db0212
John Seo <shostga@naver.com>
2024-10-09 21:48:06 +0900

    revise RegisterPage.js

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { useState } from "react";
 import api from "../utils/api";
 
 const RegisterPage = () => {
