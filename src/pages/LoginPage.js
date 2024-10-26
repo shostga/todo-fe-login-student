be61a0e907807af7d14155205288968f9997386d
John Seo <shostga@naver.com>
2024-10-26 23:10:24 +0900

    revise LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await api.post("/user/login", { email, password });
+      console.log("rrrrrr: ", response);
+    } catch (error) {}
+  };
+
   return (
     <div className="display-center">
-      <Form className="login-box">
+      <Form className="login-box" onSubmit={handleLogin}>
         <h1>로그인</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </Form.Group>
         <div className="button-box">
           <Button type="submit" className="button-primary">
